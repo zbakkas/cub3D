29f43b0cc7133c9fc77ea571ebb625d48c086301
Zouhirborali <zouhirbakkas@gmail.com>
2024-10-01 18:12:45 +0100

    norm

--- door.c
+++ door.c
@@ -6,78 +6,94 @@
 /*   By: zbakkas <zouhirbakkas@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 13:27:45 by zbakkas           #+#    #+#             */
-/*   Updated: 2024/10/01 13:35:00 by zbakkas          ###   ########.fr       */
+/*   Updated: 2024/10/01 18:10:19 by zbakkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void render_door(t_player  *player,int i,double ray_angle,int flag,t_intersection intersection)
+typedef struct s_arg_door
 {
-    double wall_height;
-    double wall_b;
-    double wall_t;
-    double sq;
-    bool    check;
-    t_int   color;
-    double  tmp;
+	double	wall_height;
+	double	wall_b;
+	double	wall_t;
+	double	sq;
+	bool	check;
+	t_int	color;
+	double	tmp;
+}t_arg_door;
 
-    check = false;
-    player->texture_offset = 0;
-    intersection.distance *= cos(ray_angle - player->angle);
-    wall_height =(PEX/intersection.distance)*(WIDTH/2)/tan(FOV/2);
-    wall_b= ((HEIGHT/2)+(wall_height/2)) +player->yy;
-    wall_t= ((HEIGHT/2)-(wall_height/2))+player->yy;
-    if(wall_b> HEIGHT) 
-        wall_b =HEIGHT;
-    if(wall_t<0)
-    {   
-        check = true;
-        tmp = (-wall_t) / wall_height;
-        wall_t=0;
-    }
-    sq = wall_t;
-    player->wall_height = wall_height;
-    player->wall_t = wall_t;
-    player->is_vertical = flag;
-    if(player->map[(int)(intersection.y/PEX)][(int)(intersection.x/PEX)]=='d')
-    {
-        while (wall_t< wall_b)
-        {
-            player->pos_y = wall_t;
-            if (check)
-                player->texture_offset = tmp * player->door_open_tex->height;
-            color = load_colors(player, player->door_open_tex, (t_fpoint){intersection.x, intersection.y});
-            if(color)
-                mlx_put_pixel(player->img,i,wall_t, color);
-            wall_t++;
-        }
-    }
+void	render_door_tow(t_player *player, t_arg_door	arg,
+t_intersection intersection, int i)
+{
+	if (player->map[(int)(intersection.y / PEX)]
+		[(int)(intersection.x / PEX)] == 'd')
+	{
+		while (arg.wall_t < arg.wall_b)
+		{
+			player->pos_y = arg.wall_t;
+			if (arg.check)
+				player->texture_offset = arg.tmp
+					* player->door_open_tex->height;
+			arg.color = load_colors(player, player->door_open_tex,
+					(t_fpoint){intersection.x, intersection.y});
+			if (arg.color)
+				mlx_put_pixel(player->img, i, arg.wall_t, arg.color);
+			arg.wall_t++;
+		}
+	}
 }
 
-int check_door(t_player *player, int distance)
+void	render_door(t_player *player, int i, double ray_angle, int flag, t_intersection intersection)
 {
-    int i;
-    double x;
-    double y;
-    
-    i = 1;
-    while (i<= distance)
-    {
-        x =player->x + cos(normal_ang(player->angle))*i;
-        y = player->y + sin(normal_ang(player->angle))*i;
-        if(player->map[(int)(y/PEX)][(int)(x/PEX)]=='D' && player->map[(int)(player->y/PEX)][(int)(player->x/PEX)]!='D')
-        {
-            player->map[(int)(y/PEX)][(int)(x/PEX)]='d';
-            return 1;
-        }
-        else if(player->map[(int)(y/PEX)][(int)(x/PEX)]=='d'&& player->map[(int)(player->y/PEX)][(int)(player->x/PEX)]!='d')
-        {
-            player->map[(int)(y/PEX)][(int)(x/PEX)]='D';
-            return 2;
-        }
-        i++;
-    }
+	t_arg_door	arg;
 
-    return 0;
+	arg.check = false;
+	player->texture_offset = 0;
+	intersection.distance *= cos(ray_angle - player->angle);
+	arg.wall_height = (PEX / intersection.distance)
+		* (WIDTH / 2) / tan(FOV / 2);
+	arg.wall_b = ((HEIGHT / 2) + (arg.wall_height / 2)) + player->yy;
+	arg.wall_t = ((HEIGHT / 2) - (arg.wall_height / 2)) + player->yy;
+	if (arg.wall_b > HEIGHT) 
+		arg.wall_b = HEIGHT;
+	if (arg.wall_t < 0)
+	{
+		arg.check = true;
+		arg.tmp = (-arg.wall_t) / arg.wall_height;
+		arg.wall_t = 0;
+	}
+	arg.sq = arg.wall_t;
+	player->wall_height = arg.wall_height;
+	player->wall_t = arg.wall_t;
+	player->is_vertical = flag;
+	render_door_tow(player, arg, intersection, i);
+}
+
+int	check_door(t_player *player, int distance)
+{
+	int		i;
+	double	x;
+	double	y;
+
+	i = 1;
+	while (i <= distance)
+	{
+		x = player->x + cos(normal_ang(player->angle)) * i;
+		y = player->y + sin(normal_ang(player->angle)) * i;
+		if (player->map[(int)(y / PEX)][(int)(x / PEX)] == 'D' 
+		&& player->map[(int)(player->y / PEX)][(int)(player->x / PEX)] != 'D')
+		{
+			player->map[(int)(y / PEX)][(int)(x / PEX)] = 'd';
+			return (1);
+		}
+		else if (player->map[(int)(y / PEX)][(int)(x / PEX)] == 'd'
+		&& player->map[(int)(player->y / PEX)][(int)(player->x / PEX)] != 'd')
+		{
+			player->map[(int)(y / PEX)][(int)(x / PEX)] = 'D';
+			return (2);
+		}
+		i++;
+	}
+	return (0);
 }
