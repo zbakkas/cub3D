80e9b456f7032170580267720b45a1f63d633a12
Zouhirborali <zouhirbakkas@gmail.com>
2024-10-01 17:13:04 +0100

    norm

--- intersection.c
+++ intersection.c
@@ -6,85 +6,84 @@
 /*   By: zbakkas <zouhirbakkas@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 13:10:04 by zbakkas           #+#    #+#             */
-/*   Updated: 2024/10/01 13:26:24 by zbakkas          ###   ########.fr       */
+/*   Updated: 2024/10/01 16:22:28 by zbakkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-
-void int__(t_player *player ,t_intersection *int_ , bool is_door, double xa, double ya)
+void	int__(t_player *player, t_intersection *int_, bool is_door, double xa, double ya)
 {
-    while ((int)(int_->x/PEX) >=0 && (int)(int_->x/PEX) <= player->map_weidth && (int)(int_->y/PEX) >=0 && (int)(int_->y/PEX)<=player->map_height)
-    {
-        if((int)(int_->y/PEX) < player->map_height &&((int)(int_->x/PEX)) < (int)ft_strlen(player->map[(int)(int_->y/PEX)]) && (player->map[(int)(int_->y/PEX)][(int)(int_->x/PEX)]=='1'|| (player->map[(int)(int_->y/PEX)][(int)(int_->x/PEX)]=='D' || (player->map[(int)(int_->y/PEX)][(int)(int_->x/PEX)]=='d'))))
-        {
-            if(player->map[(int)(int_->y/PEX)][(int)(int_->x/PEX)] == 'd' && is_door)
-            {
-                int_->x+=xa;
-                int_->y+=ya;
-            }
-            else
-            {
-                int_->distance = (sqrt(pow(int_->x - player->x, 2) + pow(int_->y - player->y, 2)));
-                break;
-            }
-        }
-        else
-        {
-            int_->x+=xa;
-            int_->y+=ya;
-        }
-    }
+	while ((int)(int_->x/PEX) >=0 && (int)(int_->x/PEX) <= player->map_weidth && (int)(int_->y/PEX) >=0 && (int)(int_->y/PEX)<=player->map_height)
+	{
+		if ((int)(int_->y/PEX) < player->map_height &&((int)(int_->x/PEX)) < (int)ft_strlen(player->map[(int)(int_->y/PEX)]) && (player->map[(int)(int_->y/PEX)][(int)(int_->x/PEX)]=='1'|| (player->map[(int)(int_->y/PEX)][(int)(int_->x/PEX)]=='D' || (player->map[(int)(int_->y/PEX)][(int)(int_->x/PEX)]=='d'))))
+		{
+			if (player->map[(int)(int_->y/PEX)][(int)(int_->x/PEX)] == 'd' && is_door)
+			{
+				int_->x += xa;
+				int_->y += ya;
+			}
+			else
+			{
+				int_->distance = (sqrt(pow(int_->x - player->x, 2) + pow(int_->y - player->y, 2)));
+				break ;
+			}
+		}
+		else
+		{
+			int_->x += xa;
+			int_->y += ya;
+		}
+	}
 }
 
-t_intersection get_h(t_player *player, double ray_angle, bool is_door)
+t_intersection	get_h(t_player *player, double ray_angle, bool is_door)
 {
-    t_intersection int_;
-    double first_y;
-    double first_x;
-    double ya;
-    double xa;
-    
-    int_.distance =99999999999999;
-    if(ray_angle > 0 && ray_angle < M_PI)//down
-        first_y =(floor(player->y/PEX)*PEX)+PEX;
-    else    
-        first_y =(floor(player->y/PEX) *PEX)-0.00001;
-    first_x = player->x+((first_y-player->y)/tan(ray_angle));
-    int_.x = first_x;
-    int_.y = first_y;
-    if(ray_angle > 0 && ray_angle < M_PI)
-        ya=PEX;
-    else    
-        ya=-PEX;
-    xa = ya/tan(ray_angle);
-   int__(player,&int_,is_door,xa,ya);
-    return int_;
+	t_intersection	int_;
+	double			first_y;
+	double			first_x;
+	double			ya;
+	double			xa;
+
+	int_.distance = 99999999999999;
+	if (ray_angle > 0 && ray_angle < M_PI)
+		first_y = (floor(player->y / PEX) * PEX) + PEX;
+	else
+		first_y = (floor(player->y / PEX) * PEX) - 0.00001;
+	first_x = player->x + ((first_y - player->y) / tan(ray_angle));
+	int_.x = first_x;
+	int_.y = first_y;
+	if (ray_angle > 0 && ray_angle < M_PI)
+		ya = PEX;
+	else
+		ya = -PEX;
+	xa = ya / tan(ray_angle);
+	int__(player, &int_, is_door, xa, ya);
+	return (int_);
 }
 
 t_intersection get_v(t_player *player ,double ray_angle , bool is_door)
 {
-    t_intersection int_;
-    double first_x;
-    double first_y;
-    double xa;
-    double ya;
-    
-    int_.distance =99999999999999;
-      // if(ray_angle < (0.5*M_PI) || ray_angle >(1.5*M_PI))//rigth
-    if(!(ray_angle > M_PI / 2 && ray_angle < 3 * M_PI / 2))
-        first_x=(floor(player->x/PEX)*PEX)+PEX;
-    else    
-        first_x = (floor(player->x/PEX)*PEX)-0.001;
-    first_y = player->y+( first_x -player->x)*tan(ray_angle);
-    int_.x =first_x;
-    int_.y =first_y;
-     if(!(ray_angle > M_PI / 2 && ray_angle < 3 * M_PI / 2))
-        xa =PEX;
-    else
-        xa=-PEX;
-    ya= xa*tan(ray_angle);
-    int__(player,&int_,is_door,xa,ya);
-    return int_;
+	t_intersection	int_;
+	double			first_x;
+	double			first_y;
+	double			xa;
+	double			ya;
+
+	int_.distance = 99999999999999;
+	  // if(ray_angle < (0.5*M_PI) || ray_angle >(1.5*M_PI))//rigth
+	if (!(ray_angle > M_PI / 2 && ray_angle < 3 * M_PI / 2))
+		first_x = (floor(player->x / PEX) * PEX) + PEX;
+	else
+		first_x = (floor(player-> x / PEX) * PEX) - 0.001;
+	first_y = player->y + (first_x - player->x) * tan(ray_angle);
+	int_.x = first_x;
+	int_.y = first_y;
+	if (!(ray_angle > M_PI / 2 && ray_angle < 3 * M_PI / 2))
+		xa = PEX;
+	else
+		xa = -PEX;
+	ya = xa * tan(ray_angle);
+	int__(player, &int_, is_door, xa, ya);
+	return (int_);
 }
---
+++ mainn.c
@@ -0,0 +1,130 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mainn.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zbakkas <zouhirbakkas@gmail.com>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/14 18:54:47 by zbakkas           #+#    #+#             */
+/*   Updated: 2024/10/01 16:07:07 by zbakkas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+float	normal_ang(double angle)
+{
+	angle = fmod(angle, M_PI * 2);
+	if (angle < 0)
+		angle = M_PI * 2 + angle;
+	return (angle);
+}
+
+static void	draw_rays(t_player *player)
+{
+	int				i;
+	t_intersection	distance_h;
+	t_intersection	distance_v;
+	float			ray_angle;
+
+	i = 0;
+	ray_angle = (normal_ang(player->angle) - FOV / 2);
+	while (i < NUM_RAYS)
+	{
+		ray_angle = normal_ang(ray_angle);
+		distance_h = get_h(player, ray_angle, 1);
+		distance_v = get_v(player, ray_angle, 1);
+		if (distance_v.distance < distance_h.distance)
+			render_wall(player, i, ray_angle, 1, distance_v);
+		else
+			render_wall(player, i, ray_angle, 0, distance_h);
+		distance_h = get_h(player, ray_angle, 0);
+		distance_v = get_v(player, ray_angle, 0);
+		if (distance_v.distance < distance_h.distance)
+			render_door(player, i, ray_angle, 1, distance_v);
+		else
+			render_door(player, i, ray_angle, 0, distance_h);
+		i++;
+		ray_angle += (FOV / NUM_RAYS);
+	}
+}
+
+static void	jump(t_player *player)
+{
+	if (player->space && player->time_space >= 0  && !player->player_up)
+	{
+		player->yy += 15;
+		player->time_space--;
+	}
+	if (player->space && player->time_space >= 0  && player->player_up)
+	{
+		player->yy -= 15;
+		player->time_space--;
+	}
+	if (player->time_space < 0 && !player->player_up)
+	{
+		player->player_up = true;
+		player->time_space = 12;
+	}
+	if (player->time_space < 0 && player->player_up && player->space)
+	{
+		player->player_up = false;
+		player->space = false;
+		player->time_space = 12;
+	}
+}
+
+static void	game_loop(void *param)
+{
+	t_player	*player;
+
+	player = (t_player *)param;
+	mlx_delete_image(player->mlx, player->img);
+	player->img = mlx_new_image(player->mlx, 1500, 1200);
+	mlx_image_to_window(player->mlx, player->img, 0, 0);
+	mlx_key_hook(player->mlx, my_keyhook, player);
+	jump(player);
+	draw_rays(player);
+	mini_map(player);
+	if (player->start_mouse-- < 0)
+	{
+		f_mouse(player);
+		player->start_mouse = -1;
+	}
+	else
+		mlx_set_mouse_pos(player->mlx, WIDTH / 2, HEIGHT / 2);
+	if (player->is_fire)
+		fire_(player);
+	else
+		draw_gun(player, player->gun_texture[0]);
+}
+
+int	main(int arc, char **arv)
+{
+	t_player	player;
+	///
+	player.yy =0;
+	player.i_fire =0;
+	player.i_time =0;
+	player.space =false;
+	player.time_space = 12;
+	player.player_down =true;
+	player.player_up =false;
+	player.start_mouse =15;
+///
+	if (arc == 2)
+		init_all_data(arv, &player);
+	else
+	{
+		ft_putendl_fd("Use ./cub3D file.cub", STDERR_FILENO);
+		exit(0);
+	}
+	player.mlx = mlx_init(WIDTH, HEIGHT, "cub3D", false);
+	player.gun_image = mlx_new_image(player.mlx, WIDTH, HEIGHT);
+	player.img = mlx_new_image(player.mlx, WIDTH, HEIGHT);
+	mlx_image_to_window(player.mlx, player.img, 0, 0);
+	mlx_set_cursor_mode(player.mlx, MLX_MOUSE_HIDDEN);
+	mlx_loop_hook(player.mlx, game_loop, &player);
+	mlx_loop(player.mlx);
+	return (0);
+}
