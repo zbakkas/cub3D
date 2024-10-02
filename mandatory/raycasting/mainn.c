a07a47ba86f662b3dafaba0db00d913450179d10
Zouhirborali <zouhirbakkas@gmail.com>
2024-10-02 16:10:43 +0100

    add

--- mandatory/raycasting/mainn.c
+++ mandatory/raycasting/mainn.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   mainn.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hel-bouk <hel-bouk@student.42.fr>          +#+  +:+       +#+        */
+/*   By: zbakkas <zouhirbakkas@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/14 18:54:47 by zbakkas           #+#    #+#             */
-/*   Updated: 2024/10/02 12:08:48 by hel-bouk         ###   ########.fr       */
+/*   Updated: 2024/10/02 15:52:09 by zbakkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,38 +37,7 @@ static void	draw_rays(t_player *player)
 			render_wall(player, args, 1, distance_v);
 		else
 			render_wall(player, args, 0, distance_h);
-		distance_h = get_h(player, args.ray_angle, 0);
-		distance_v = get_v(player, args.ray_angle, 0);
-		if (distance_v.distance < distance_h.distance)
-			render_door(player, args, 1, distance_v);
-		else
-			render_door(player, args, 0, distance_h);
 		args.ray_angle += (FOV / NUM_RAYS);
-	}
-}
-
-static void	jump(t_player *player)
-{
-	if (player->space && player->time_space >= 0 && !player->player_up)
-	{
-		player->yy += 15;
-		player->time_space--;
-	}
-	if (player->space && player->time_space >= 0 && player->player_up)
-	{
-		player->yy -= 15;
-		player->time_space--;
-	}
-	if (player->time_space < 0 && !player->player_up)
-	{
-		player->player_up = true;
-		player->time_space = 12;
-	}
-	if (player->time_space < 0 && player->player_up && player->space)
-	{
-		player->player_up = false;
-		player->space = false;
-		player->time_space = 12;
 	}
 }
 
@@ -81,20 +50,7 @@ static void	game_loop(void *param)
 	player->img = mlx_new_image(player->mlx, 1500, 1200);
 	mlx_image_to_window(player->mlx, player->img, 0, 0);
 	mlx_key_hook(player->mlx, my_keyhook, player);
-	jump(player);
 	draw_rays(player);
-	mini_map(player);
-	if (player->start_mouse-- < 0)
-	{
-		f_mouse(player);
-		player->start_mouse = -1;
-	}
-	else
-		mlx_set_mouse_pos(player->mlx, WIDTH / 2, HEIGHT / 2);
-	if (player->is_fire)
-		fire_(player);
-	else
-		draw_gun(player, player->gun_texture[0]);
 }
 
 int	main(int arc, char **arv)
@@ -117,7 +73,6 @@ int	main(int arc, char **arv)
 		exit(0);
 	}
 	player.mlx = mlx_init(WIDTH, HEIGHT, "cub3D", false);
-	player.gun_image = mlx_new_image(player.mlx, WIDTH, HEIGHT);
 	player.img = mlx_new_image(player.mlx, WIDTH, HEIGHT);
 	mlx_image_to_window(player.mlx, player.img, 0, 0);
 	mlx_set_cursor_mode(player.mlx, MLX_MOUSE_HIDDEN);
